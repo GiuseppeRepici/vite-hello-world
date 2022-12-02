4facb09b5803fa3460d2d216a03d0bdaa1d4c64d
GiuseppeRepici <giuseppe.repici95@gmail.com>
2022-12-02 16:53:12 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import AppTitleVue from './components/AppTitle.vue';
+</script>
+
+<template>
+  <div>
+    <AppTitleVue/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>La mia prima app con vite</h1>
+</template>
